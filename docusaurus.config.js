ca370878a6453fe0082157f7583a25891bab9cc2
pierrot <pierrot@cs2tools.info>
2024-01-25 00:09:23 +0000

    Update positions and favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 const config = {
   title: "UoL CS Docs",
   tagline: "Dinosaurs are cool",
-  favicon: "img/favicon.ico",
+  favicon: "img/my-uol-logo-no-bg-no-border.png",
 
   // Set the production url of your site here
   url: "https://your-docusaurus-site.example.com",
